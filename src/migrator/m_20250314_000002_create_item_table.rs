9d97c7dc9fd29f13546499184a1b45c7b74573f7
Eb-dur <malte.hammar01@gmail.com>
2025-03-14 18:18:17 +0100

    Add item table to database

---
+++ src/migrator/m_20250314_000002_create_item_table.rs
@@ -0,0 +1,45 @@
+use sea_orm_migration::prelude::*;
+
+pub struct Migration;
+
+impl MigrationName for Migration {
+    fn name(&self) -> &str {
+        "m_20250314_000002_create_item_table."
+    }
+}
+
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager : &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Item::Table)
+                    .col(
+                        ColumnDef::new(Item::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Item::Type)
+                        .string_len(255)
+                        .not_null())
+                    .to_owned(),
+            ).await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Item::Table).to_owned())
+            .await
+    }
+}
+
+    #[derive(Iden)]
+    pub enum Item{
+        Table,
+        Id,
+        Type,
+    }
